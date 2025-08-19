3fc92d3d72c88e049b9e1c79440155130111e880
Thais Nascimento <thaisn.design@gmail.com>
2025-08-19 14:31:03 +0100

    Update ft_putnbr.c

--- c04/ex02/ft_putnbr.c
+++ c04/ex02/ft_putnbr.c
@@ -17,35 +17,29 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void ft_putstr(char *str)
-{
-	while(*str)
-	{
-		ft_putchar(*str);
-		str++;
-	}
-}
-
 void	ft_putnbr(int nb)
 {
-	if (nb < 0)
+	if (nb == -2147483648)
 	{
 		ft_putchar('-');
-		if (nb == -2147483648)
-		{
-			ft_putstr("2147483648");
-			return;
-		}
-		nb = -nb;
+		ft_putchar('2');
+		ft_putnbr(147483648);
 	}
-	if (nb > 9)
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+		ft_putnbr(nb);
+	}
+	else if (nb > 9)
 	{
 		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
 	}
-	ft_putchar(nb % 10 + 48);
-	
+	else
+		ft_putchar(nb + 48);
 }
-
+/*
 int main(void)
 {
 	ft_putnbr(42); // 42
@@ -56,3 +50,4 @@ int main(void)
 	ft_putchar('\n');
 	return (0);
 }
+*/
